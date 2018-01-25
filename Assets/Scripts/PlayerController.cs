0e319bcc9a9c30547a170a0e31800f7903b5c80f
brianphipps <phipps@lovetoknow.com>
2018-01-24 17:25:00 -0800

    initial commit of Owens game

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody2D rb2d;
+	private int count;
+	public Text countText;
+	public Text winText;
+
+	void Start()
+	{
+		rb2d = GetComponent<Rigidbody2D>();
+		count = 0;
+		winText.text = "";
+		SetCountText ();
+
+
+	}
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		rb2d.AddForce (movement * speed);
+
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("PickUp")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+			Debug.Log(winText.text);
+		}
+
+
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count:" + count.ToString ();
+		if (count == 9)
+		{
+			winText.text = "YOU WIN!!";			
+		} else if(count == 1)
+		{
+			winText.text = "Good Job";
+		} else if(count == 2)
+		{
+			winText.text = "";
+		} else if(count == 8)
+		{
+			winText.text = "You've Almost Won";
+		}
+	}
+
+
+
+}
---
+++ Assets/Scripts/testActive.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testActive : MonoBehaviour {
+
+	//public GameObject GO;
+
+	public bool allOptions = true;
+	public bool extended1 = true;
+	public bool extended2 = true;
+
+	void OnGUI()
+	{
+//		if (GUI.Button(new Rect(10, 10, 100, 30), "Enable"))
+//		{
+//			Debug.Log("Enable: " + GO.name);
+//			GO.SetActive(true);
+//		}
+//		if (GUI.Button(new Rect(10, 50, 100, 30), "Disable"))
+//		{
+//			Debug.Log("Disable: " + GO.name);
+//			GO.SetActive(false);
+//		}
+
+	
+			allOptions = GUI.Toggle(new Rect(0, 0, 150, 20), allOptions, "Edit All Options");
+			GUI.enabled = allOptions;
+			extended1 = GUI.Toggle(new Rect(20, 20, 130, 20), extended1, "Extended Option 1");
+			extended2 = GUI.Toggle(new Rect(20, 40, 130, 20), extended2, "Extended Option 2");
+			GUI.enabled = true;
+			if (GUI.Button(new Rect(0, 60, 150, 20), "Ok"))
+				print("user clicked ok");
+			
+	}
+
+}
